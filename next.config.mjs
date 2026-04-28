dc4c1f05f36d17004ff3e1ee891211e8a93b73f6
cocostyle <dresscoco2@gmail.com>
2026-04-28 03:10:46 -0400

    fix vercel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
+const nextConfig = { images: { domains: ['images.unsplash.com'] }, eslint: { ignoreDuringBuilds: true }, typescript: { ignoreBuildErrors: true } };
 export default nextConfig;
